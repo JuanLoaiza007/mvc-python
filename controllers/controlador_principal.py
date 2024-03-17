2189c2892e9d5310df90a83f45f14cab3810df40
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-17 12:10:52 -0500

    default maximum size patch v2

--- controllers/controlador_principal.py
+++ controllers/controlador_principal.py
@@ -12,6 +12,8 @@ class controlador_principal:
     def cargar(self, main_window):
         self.modelo = modelo_principal()
         self.MainWindow = main_window
+        self.MainWindow.setMinimumSize(self.MainWindow.minimumSizeHint())
+        self.MainWindow.setMaximumSize(16777215, 16777215)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.MainWindow)
 
@@ -35,21 +37,19 @@ class controlador_principal:
         saludo = "Hola " + self.ui.txta_nombre.toPlainText()
         new_ui.lbl_main_text.setText(saludo)
 
-        copy_mwindow = self.MainWindow
-        self.block_focus(copy_mwindow)
+        self.block_focus(self.MainWindow)
         new_dialog.exec()
 
         self.ui.txta_nombre.setText("")
-        self.unblock_focus(copy_mwindow)
+        self.unblock_focus()
 
     def block_focus(self, window):
         self.MainWindow.setEnabled(False)
         self.MainWindow.setFixedSize(window.size())
 
-    def unblock_focus(self, window):
+    def unblock_focus(self):
         self.MainWindow.setMinimumSize(self.MainWindow.minimumSizeHint())
         self.MainWindow.setMaximumSize(16777215, 16777215)
-
         self.MainWindow.setEnabled(True)
 
     def mostrar_sobre_nosotros(self):
